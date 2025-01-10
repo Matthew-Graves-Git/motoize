0c61e891dfd45e2419c7b662397119eed46cd0ff
Matthew-Graves-Git <matthew.graves.cs@gmail.com>
2025-01-10 18:47:55 -0500

    fixed html elements, more clear

--- src/Pages/Landing/About.jsx
+++ src/Pages/Landing/About.jsx
@@ -2,10 +2,10 @@ import aboutImg from '../../Assets/about_img.jpg';
 import InfoContainer from '../../Components/InfoContainer';
 const About = () => {
     return ( 
-        <div className='z-10 h-screen w-screen sticky top-0 overflow-clip '>
+        <section className='z-10 h-screen w-screen sticky top-0 overflow-clip '>
             <InfoContainer/>
             <img src={aboutImg} className='h-full w-screen object-cover object-[55%]' alt="" />
-        </div>
+        </section>
      );
 }
  
--- src/Pages/Landing/Hero.jsx
+++ src/Pages/Landing/Hero.jsx
@@ -2,10 +2,10 @@ import heroImg from '../../Assets/hero_img.jpg';
 import InfoContainer from '../../Components/InfoContainer';
 const Hero = () => {
     return ( 
-        <div className='h-screen w-screen sticky top-0 overflow-clip'>
+        <section className='h-screen w-screen sticky top-0 overflow-clip'>
             <InfoContainer/>
             <img src={heroImg} className='h-full w-screen object-cover object-[30%]' alt="" />
-        </div>
+        </section>
      );
 }
  
