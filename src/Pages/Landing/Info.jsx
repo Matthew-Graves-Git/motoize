e8cd6df648d1818754dbba49b8ce0ccdd97196ca
Matthew-Graves-Git <matthew.graves.cs@gmail.com>
2025-01-18 10:13:58 -0500

    hover image changes

--- src/Pages/Landing/Info.jsx
+++ src/Pages/Landing/Info.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import familyImg from "../../Assets/family-info.webp";
 import familyOutdoorImg from "../../Assets/family-info2.webp";
 const Info = () => {
+    const [currentImg, setCurrentImg] = useState(familyImg);
+
+    const handleHover = (e) => {
+        const id =  (e.target.tagName.toLowerCase() === 'span') ? e.target.id : e.target.parentElement.id;
+        if(id === "1"){
+            setCurrentImg(familyImg);
+        }
+        if(id === "2"){
+            setCurrentImg(familyOutdoorImg);
+        }
+    }
+
     return ( 
         <section className="bg-white h-full w-screen relative z-20">
             <div className="py-40 overflow-clip">
-                <img className="h-[28rem] lg:w-[70%] w-auto object-cover mx-auto px-8 mb-20" src={familyImg} alt="" />
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-8 lg:w-[70%] w-auto mx-auto group">
-                    <span className="flex flex-col gap-6 border-t-2 py-6 group-hover:border-green-950 group-hover:scale-105 cursor-pointer transition duration-500" >
+                <img className="h-[28rem] lg:w-[70%] w-auto object-cover mx-auto px-8 mb-20" src={currentImg} alt="" />
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-8 lg:w-[70%] w-auto mx-auto">
+                    <div  onMouseOver={(e)=>handleHover(e)} className="group">
+                    <span id="1" className="flex flex-col gap-6 border-t-2 py-6 group-hover:border-green-950 group-hover:scale-105 cursor-pointer transition duration-500" >
                         <h3 className="font-bold">
                             Reasons to Love Ascent
                         </h3>
@@ -24,7 +38,9 @@ const Info = () => {
                             Link Here
                         </p>
                     </span>
-                    <span className="flex flex-col gap-6 border-t-2 py-6" >
+                    </div>
+                    <div  onMouseOver={(e)=>handleHover(e)} className="group">
+                    <span id="2"  className="flex flex-col gap-6 border-t-2 py-6 group-hover:border-green-950 group-hover:scale-105 cursor-pointer transition duration-500" >
                         <h3 className="font-bold">
                             Reasons to Love Ascent
                         </h3>
@@ -42,6 +58,7 @@ const Info = () => {
                             Link Here
                         </p>
                     </span>
+                    </div>
                 </div>
             </div>
         </section>
